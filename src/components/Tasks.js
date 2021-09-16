a2d7a9a542089d92916b6f5a51c1e1b295b47d8a
Eduardo Guereque <edwardgnt@gmail.com>
2021-09-16 15:32:50 -0700

    initial commit

---
+++ src/components/Tasks.js
@@ -0,0 +1,31 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'April 4th at 3pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'May 6th at 1pm',
+        reminder: false,
+    },
+
+]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
